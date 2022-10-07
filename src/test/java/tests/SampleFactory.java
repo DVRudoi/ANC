425e3b6714e821b7e8905cc429b302d7e652c1c7
DVRudoi <dvrudoy@gmail.com>
2022-10-07 22:18:17 +0300

    XML2

---
+++ src/test/java/tests/SampleFactory.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
+
+public class SampleFactory {
+
+    @Factory(dataProvider="dp")
+    public Object[] createInstances(String str) {
+        return new Object[] {new StartTest(str)};
+    }
+    @DataProvider(name="dp")
+    public static Object[][] createData() {
+        return new Object[][] {
+                new Object[] { new String("Біфрен") },
+                new Object[] { new String("Ангіноваг") }
+        };
+    }
+
+}
